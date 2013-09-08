ecc4415124b4f32bcc8f6aa9092aac0620bb4071
D420 <dawsey420@gmail.com>
2013-09-08 14:57:51 +0200

    Transfiguracion 5.0 Rochet2.

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1389,6 +1389,7 @@ void AddBattlegroundScripts()
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
 
+void AddSC_CPWS_Transmogrification();
 #endif
 
 void AddCustomScripts()
@@ -1396,5 +1397,6 @@ void AddCustomScripts()
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
 
+    AddSC_CPWS_Transmogrification();
 #endif
 }
