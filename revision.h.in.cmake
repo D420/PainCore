b6bf41b78ead6411eebf3cde767ebc8be5675bb4
D420 <dawsey420@gmail.com>
2013-09-08 19:36:26 +0200

    Version PainCore.

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -2,11 +2,11 @@
 #define __REVISION_H__
  #define _HASH                      "0.0.1"
  #define _DATE                      "08/09/2013"
- #define _BRANCH                    "@rev_branch@"
+ #define _BRANCH                    "master"
  #define VER_COMPANYNAME_STR        "TrinityCore Developers"
  #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2013 TrinityCore"
  #define VER_FILEVERSION            0,0,0
- #define VER_FILEVERSION_STR        "@rev_hash@ @rev_date@ (@rev_branch@ branch)"
+ #define VER_FILEVERSION_STR        "0.0.1 08/09/2013 (master branch)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
  #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
 #endif // __REVISION_H__
