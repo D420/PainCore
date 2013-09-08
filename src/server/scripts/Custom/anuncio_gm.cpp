177f8c6efec853f7016f6e426eb2c004f965fcfc
D420 <dawsey420@gmail.com>
2013-09-09 00:40:57 +0200

    Fix GM Login.

--- src/server/scripts/Custom/anuncio_gm.cpp
+++ src/server/scripts/Custom/anuncio_gm.cpp
@@ -10,7 +10,6 @@ public:
 	{
 			if (player->GetSession()->GetSecurity() >= SEC_MODERATOR)
 			{
-				ChatHandler handler(player);
 				uint16 gmlvl = player->GetSession()->GetSecurity();
 				std::string gmname = player->GetName();
 				uint32 playeronline = sWorld->GetPlayerCount();
@@ -18,14 +17,14 @@ public:
 				std::string player_ip = player->GetSession()->GetRemoteAddress();
 				uint16 tickets = sTicketMgr->GetOpenTicketCount();
 
-				handler.PSendSysMessage("|cffff0000=================================|r");
-				handler.PSendSysMessage("|cff00ff00Bienvenido,|r %s", gmname.c_str());
-				handler.PSendSysMessage("|cff00ff00Tu nivel de cuenta es:|r %u", gmlvl);
-				handler.PSendSysMessage("|cff00ff00Tu direcci""\xC3\xB3""n IP es:|r %s", player_ip.c_str()); 
-				handler.PSendSysMessage("|cff00ff00En este momento hay|r %u |cff00ff00jugadores en l""\xC3\xAD""nea|r", playeronline);
-				handler.PSendSysMessage("|cff00ff00Tickets abiertos:|r %u", tickets);
-				handler.PSendSysMessage("|cff00ff00Servidor Online durante:|r %s", uptime.c_str());
-				handler.PSendSysMessage("|cffff0000=================================|r");
+				ChatHandler(player->GetSession()).PSendSysMessage("|cffff0000=================================|r");
+				ChatHandler(player->GetSession()).PSendSysMessage("|cff00ff00Bienvenido,|r %s", gmname.c_str());
+				ChatHandler(player->GetSession()).PSendSysMessage("|cff00ff00Tu nivel de cuenta es:|r %u", gmlvl);
+				ChatHandler(player->GetSession()).PSendSysMessage("|cff00ff00Tu direcci""\xC3\xB3""n IP es:|r %s", player_ip.c_str()); 
+				ChatHandler(player->GetSession()).PSendSysMessage("|cff00ff00En este momento hay|r %u |cff00ff00jugadores en l""\xC3\xAD""nea|r", playeronline);
+				ChatHandler(player->GetSession()).PSendSysMessage("|cff00ff00Tickets abiertos:|r %u", tickets);
+				ChatHandler(player->GetSession()).PSendSysMessage("|cff00ff00Servidor Online durante:|r %s", uptime.c_str());
+				ChatHandler(player->GetSession()).PSendSysMessage("|cffff0000=================================|r");
 				return;
 			}
 	}
